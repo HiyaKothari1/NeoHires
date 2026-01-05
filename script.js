127a5cb312dcb954e483c15ed9c4cf9920e7dcd6
Hiya Kothari <hiyakothari2@gmail.com>
2026-01-05 22:16:01 +0530

    Initial commit: NeoHires project

---
+++ script.js
@@ -0,0 +1,628 @@
+const data = [
+  // JANUARY
+  {
+    month: "January",
+    company: "Uber",
+    role: "Software Engineer Intern",
+    name:"HackTag",
+    type: "Hackathon",
+    eligibility:"3rd year",
+    status: "Open",
+    logo: "assets/logos/uber.png",
+    applyLink: "https://www.uber.com/careers/"
+  },
+  {
+    month: "January",
+    company: "Uber",
+    role: "Software Engineer Intern",
+    name:"She++",
+    type: "Internship",
+    eligibility:"female",
+    status: "Closed",
+    logo: "assets/logos/uber.png",
+    applyLink: "https://flows.beamery.com/uber/apac-uber-she-2026-tjfslqitq"
+  },
+  {
+    month: "January",
+    company: "American Express",
+    role: "Software Engineer Intern",
+    name:"CodeStreet",
+    type: "Hackathon",
+    eligibility:"Btech student",
+    status: "Open",
+    logo: "assets/logos/american.png",
+    applyLink: "https://unstop.com/hackathons/uber-hacktag-20-uber-technologies-inc-260528?refId=Shiv"
+  },
+  {
+    month: "January",
+    company: "RBI",
+    name:"SDE Intern Program",
+    role: "Software Engineer Intern",
+    type: "Reserach Internship",
+    eligibility:"2nd/3rd year",
+    status: "Open",
+    logo: "assets/logos/RBI.png",
+    applyLink: "https://careers.microsoft.com/"
+  },
+
+  // FEBRUARY
+  {
+    month: "February",
+    company: "Google",
+    name:"GSOC 2026", 
+    role: "open-source",
+    type: "Internship",
+    eligibility:"2nd-3rd year",
+    status: "Upcoming",
+    logo: "assets/logos/google.png",
+    applyLink: "https://summerofcode.withgoogle.com/"
+  },
+  {
+    month: "February",
+    company: "JP Morgan",
+    name:"Code for good",
+    role: "Software Engineer Intern",
+    type: "Hackathon",
+    eligibility:"2nd-3rd year",
+    status: "Open",
+    logo: "assets/logos/JPmorgan.png",
+    applyLink: "https://www.flipkartcareers.com/"
+  },
+  {
+    month: "February",
+    company: "Walmart",
+    name:"CodeHers",
+    role: "Software Engineer Intern",
+    type: "Hackathon",
+    eligibility:"female",
+    status: "Open",
+    logo: "assets/logos/walmart.png",
+    applyLink: "https://careers.walmart.com/students"
+  },
+   {
+    month: "February",
+    company: "Servicenow",
+    name:"Women code to win",
+    role: "Software Engineer Intern",
+    type: "Hackathon",
+    eligibility:"female",
+    status: "Upcoming",
+    logo: "assets/logos/servicenow.png",
+    applyLink: "https://www.flipkartcareers.com/"
+  },
+
+  // MARCH
+    {
+    month: "March",
+    company: "Justpay",
+    name:"DEV-BE02",
+    role: "Software Engineer Intern",
+    type: "Internship",
+    eligibility:"B-Tech Student",
+    status: "Upcoming",
+    logo: "assets/logos/justpay.png",
+    applyLink: "https://joinus.juspay.in/?jobId=DEV-BE02"
+  },
+    {
+    month: "March",
+    company: "Github",
+    name:"Github Externship",
+    role: "Software Engineer Intern",
+    type: "Internship",
+    eligibility:"3rd year",
+    status: "upcoming",
+    logo: "assets/logos/github.png",
+    applyLink: "https://github.com/GitHub-Externships"
+  },
+    {
+    month: "March",
+    company: "Flipkart",
+    name:"Flipkart Runaway",
+    role: "Software Engineer Intern",
+    type: "Internship",
+    eligibility:"2nd-3rd year",
+    status: "upcoming",
+    logo: "assets/logos/flipkart.png",
+    applyLink: "https://www.flipkartcareers.com/flipkart/runway"
+  },
+   {
+    month: "March",
+    company: "Justpay",
+    name:"DEV-BE02",
+    role: "Software Engineer Intern",
+    type: "Internship",
+    eligibility:"B-Tech Student",
+    status: "Upcoming",
+    logo: "assets/logos/justpay.png",
+    applyLink: "https://joinus.juspay.in/?jobId=DEV-BE02"
+  },
+
+  // APRIL
+    {
+    month: "April",
+    company: "Myntra",
+    name:"Hacker-ramp",
+    role: "Software Engineer Intern",
+    type: "Internship",
+    eligibility:"2nd-3rd year",
+    status: "upcoming",
+    logo: "assets/logos/myntra.png",
+    applyLink: "https://careers.myntra.com/"
+  },
+    {
+    month: "April",
+    company: "Linked-in",
+    name:"Coach-ln Program",
+    role: "Software Engineer Intern",
+    type: "Internship",
+    eligibility:"2nd/3rd/4th year",
+    status: "upcoming",
+    logo: "assets/logos/linked-in.png",
+    applyLink: "https://welcomein.my.canva.site/coachin-2026"
+  },
+  
+  {
+    month: "April",
+    company: "Swiggy",
+    name:"SDE Intern Program",
+    role: "Business Analyst Intern",
+    type: "Internship",
+    eligibility:"2nd/3rd year",
+    status: "Upcoming",
+    logo: "assets/logos/swiggy.png",
+    applyLink: "https://careers.swiggy.com/"
+  },
+    {
+    month: "April",
+    company: "Netflix",
+    name:"SDE Intern Program",
+    role: "Software Engineer",
+    type: "Placement",
+    eligibility:"2nd/3rd year",
+    status: "Upcoming",
+    logo: "assets/logos/netflix.png",
+    applyLink: "https://jobs.netflix.com/"
+  },
+
+  // MAY
+  
+       {
+    month: "May",
+    company: "Microsoft",
+    name:"Microsoft Engage Program",
+    role: "Software Engineer Intern",
+    type: "Internship",
+    eligibility:"2nd year",
+    status: "upcoming",
+    logo: "assets/logos/microsoft.png",
+    applyLink: "https://careers.microsoft.com/v2/global/en/students"
+  }, 
+    {
+    month: "May",
+    company: "Cisco",
+    name:"Ideathon",
+    role: "Software Engineer Intern",
+    type: "Hackathon",
+    eligibility:"2nd /3rd year",
+    status: "upcoming",
+    logo: "assets/logos/cisco.png",
+    applyLink: "https://www.cisco.com/c/m/en_in/customer-experience-academy.html"
+  },
+  {
+    month: "May",
+    company: "Paytm",
+    name:"SDE Intern Program",
+    role: "Android Developer Intern",
+    type: "Internship",
+    eligibility:"2nd/3rd year",
+    status: "Upcoming",
+    logo: "assets/logos/paytm.png",
+    applyLink: "https://paytm.com/careers/"
+  },
+    {
+    month: "May",
+    company: "IBM",
+    name:"SDE Intern Program",
+    role: "Associate Developer",
+    type: "Placement",
+    eligibility:"2nd/3rd year",
+    status: "Upcoming",
+    logo: "assets/logos/ibm.png",
+    applyLink: "https://www.ibm.com/careers"
+  },
+
+  // JUNE
+   {
+    month: "June",
+    company: "Goldman Sachs",
+    name:"Summer Analyst Program",
+    role: "Software Engineer Intern",
+    type: "Internship",
+    eligibility:"3rd year",
+    status: "upcoming",
+    logo: "assets/logos/goldman.png",
+    applyLink: "https://www.goldmansachs.com/careers/students/programs-and-internships/americas/2026-summer-analyst-program"
+  },
+   {
+    month: "June",
+    company: "Swiggy",
+    name:"Swiggy Launchpad",
+    role: "Software Engineer Intern",
+    type: "Internship",
+    eligibility:"3rd year",
+    status: "upcoming",
+    logo: "assets/logos/swiggy.png",
+    applyLink: "https://careers.swiggy.com/#/"
+  },
+  {
+    month: "June",
+    company: "Infosys",
+    name:"SDE Intern Program",
+    role: "System Engineer",
+    type: "Placement",
+    eligibility:"2nd/3rd year",
+    status: "Open",
+    logo: "assets/logos/infosys.png",
+    applyLink: "https://www.infosys.com/careers/"
+  },
+  {
+    month: "June",
+    company: "TCS",
+    role: "Assistant System Engineer",
+    name:"SDE Intern Program",
+    type: "Placement",
+    eligibility:"2nd/3rd year",
+    status: "Upcoming",
+    logo: "assets/logos/tcs.png",
+    applyLink: "https://www.tcs.com/careers"
+  },
+  
+
+  // JULY
+   {
+    month: "July",
+    company: "Flipkart",
+    name:"Flipkart Grid Program",
+    role: "Software Engineer Intern",
+    type: "Internship",
+    eligibility:"2nd/3rd year",
+    status: "upcoming",
+    logo: "assets/logos/flipkart.png",
+    applyLink: "https://www.flipkartcareers.com/flipkart/grid"
+  },
+    {
+    month: "July",
+    company: "Justpay",
+    name:"DEV-BE02",
+    role: "Software Engineer Intern",
+    type: "Internship",
+    eligibility:"B-Tech Student",
+    status: "Upcoming",
+    logo: "assets/logos/justpay.png",
+    applyLink: "https://joinus.juspay.in/?jobId=DEV-BE02"
+  },
+  {
+    month: "July",
+    company: "Wipro",
+    role: "Project Engineer",
+    name:"SDE Intern Program",
+    type: "Placement",
+    eligibility:"2nd/3rd year",
+    status: "Open",
+    logo: "assets/logos/wipro.png",
+    applyLink: "https://careers.wipro.com/"
+  },
+  {
+    month: "July",
+    company: "Meta",
+    name:"SDE Intern Program",
+    role: "Associate Software Engineer",
+    type: "Hackathon",
+    eligibility:"B-Tech Student",
+    status: "Upcoming",
+    logo: "assets/logos/meta.png",
+    applyLink: "https://www.accenture.com/in-en/careers"
+  },
+
+  // AUGUST
+   {
+    month: "August",
+    company: "DEshaw & Co",
+    name:"Ascend",
+    role: "Software Engineer Intern",
+    type: "Internship",
+    eligibility:"females",
+    status: "Upcoming",
+    logo: "assets/logos/deshaw.png",
+    applyLink: "https://joinus.juspay.in/?jobId=DEV-BE02"
+  },
+  {
+    month: "August",
+    company: "CodeNation",
+    name:"CODE AGON",
+    role: "Software Engineer Intern",
+    type: "Internship",
+    eligibility:"2nd/3rd year",
+    status: "Upcoming",
+    logo: "assets/logos/codenation.png",
+    applyLink: "https://joinus.juspay.in/?jobId=DEV-BE02"
+  },
+  {
+    month: "August",
+    company: "JP Morgan",
+    name:"coderzz",
+    role: "Technology Analyst",
+    type: "Placement",
+    eligibility:"2nd/3rd year",
+    status: "Open",
+    logo: "assets/logos/jpmorgan.png",
+    applyLink: "https://careers.jpmorgan.com/"
+  },
+  {
+    month: "August",
+    company: "Deloitte",
+    name:"coderzz",
+    role: "Consulting Analyst",
+    type: "Placement",
+    eligibility:"2nd/3rd year",
+    status: "Upcoming",
+    logo: "assets/logos/deloitte.png",
+    applyLink: "https://www2.deloitte.com/careers"
+  },
+
+  // SEPTEMBER
+    {
+    month: "September",
+    company: "Google",
+    name:"SDE Intern Program",
+    role: " SDE Intern",
+    type: "Internship",
+    eligibility:"2nd/3rd year",
+    status: "Upcoming",
+    logo: "assets/logos/google.png",
+    applyLink: "https://www.google.com/about/careers/applications/jobs/results?q=internship"
+  },
+    {
+    month: "September",
+    company: "adobe",
+    name:"SDE Intern Program",
+    role: "Software Engineer Intern",
+    type: "Internship",
+    eligibility:"females",
+    status: "Upcoming",
+    logo: "assets/logos/adobe.png",
+    applyLink: "https://careers.adobe.com/us/en/apply?jobSeqNo=ADOBUSR158531EXTERNALENUS&step=1&stepname=personalInformation"
+  },
+  {
+    month: "September",
+    company: "Intel",
+    name:"SDE Intern Program",
+    role: "Hardware Engineer Intern",
+    type: "Internship",
+    eligibility:"2nd/3rd year",
+    status: "Open",
+    logo: "assets/logos/intel.png",
+    applyLink: "https://jobs.intel.com/"
+  },
+  {
+    month: "September",
+    company: "Qualcomm",
+    name:"SDE Intern Program",
+    role: "Embedded Systems Intern",
+    type: "Internship",
+    eligibility:"2nd/3rd year",
+    status: "Upcoming",
+    logo: "assets/logos/qualcomm.png",
+    applyLink: "https://www.qualcomm.com/company/careers"
+  },
+
+  // OCTOBER
+    {
+    month: "October",
+    company: "Deloitte",
+    name:"coderzz",
+    role: "Consulting Analyst",
+    type: "Placement",
+    eligibility:"2nd/3rd year",
+    status: "Upcoming",
+    logo: "assets/logos/deloitte.png",
+    applyLink: "https://www2.deloitte.com/careers"
+  },
+   {
+    month: "October",
+    company: "Amazon",
+    name: "Hackathon",
+    role: "Software Engineer Intern",
+    type: "Internship",
+    eligibility:"2nd/3rd year",
+    status: "Upcoming",
+    logo: "assets/logos/amazon.png",
+    applyLink: "https://careers.adobe.com/us/en/apply?jobSeqNo=ADOBUSR158531EXTERNALENUS&step=1&stepname=personalInformation"
+  },
+  {
+    month: "October",
+    company: "Meta",
+    name:"SDE Intern Program",
+    role: "Frontend Engineer Intern",
+    type: "Internship",
+    eligibility:"2nd/3rd year",
+    status: "Open",
+    logo: "assets/logos/meta.png",
+    applyLink: "https://www.metacareers.com/"
+  },
+  {
+    month: "October",
+    company: "Netflix",
+    name:"SDE Intern Program",
+    role: "Software Engineer",
+    type: "Placement",
+    eligibility:"2nd/3rd year",
+    status: "Upcoming",
+    logo: "assets/logos/netflix.png",
+    applyLink: "https://jobs.netflix.com/"
+  },
+
+  // NOVEMBER
+    {
+    month: "November",
+    company: "Intuit",
+    name:"coderzz",
+    role: "Consulting Analyst",
+    type: "Placement",
+    eligibility:"2nd/3rd year",
+    status: "Upcoming",
+    logo: "assets/logos/deloitte.png",
+    applyLink: "https://www2.deloitte.com/careers"
+  },
+    {
+    month: "November",
+    company: "Uber",
+    name:"uberStar",
+    role: "Consulting Analyst",
+    type: "Placement",
+    eligibility:"2nd/3rd year",
+    status: "Upcoming",
+    logo: "assets/logos/uber.png",
+    applyLink: "https://www2.deloitte.com/careers"
+  },
+  {
+    month: "November",
+    company: "Apple",
+    name:"SDE Intern Program",
+    role: "iOS Developer Intern",
+    type: "Internship",
+    eligibility:"2nd/3rd year",
+    status: "Open",
+    logo: "assets/logos/apple.png",
+    applyLink: "https://www.apple.com/careers/"
+  },
+  {
+    month: "November",
+    company: "Salesforce",
+    name:"SDE Intern Program",
+    role: "Software Engineer",
+    type: "Placement",
+    eligibility:"2nd/3rd year",
+    status: "Upcoming",
+    logo: "assets/logos/salesforce.png",
+    applyLink: "https://www.salesforce.com/company/careers/"
+  },
+
+  // DECEMBER
+  {
+    month: "December",
+    company: "Oracle",
+    name:"SDE Intern Program",
+    role: "Cloud Engineer Intern",
+    type: "Internship",
+    eligibility:"2nd/3rd year",
+    status: "Open",
+    logo: "assets/logos/oracle.png",
+    applyLink: "https://www.oracle.com/careers/"
+  },
+  {
+    month: "December",
+    company: "IBM",
+    name:"SDE Intern Program",
+    role: "Associate Developer",
+    type: "Placement",
+    eligibility:"2nd/3rd year",
+    status: "Upcoming",
+    logo: "assets/logos/ibm.png",
+    applyLink: "https://www.ibm.com/careers"
+  },
+    {
+    month: "December",
+    company: "Amazon",
+    name:"coderzz",
+    role: "Consulting Analyst",
+    type: "Placement",
+    eligibility:"2nd/3rd year",
+    status: "Upcoming",
+    logo: "assets/logos/amazon.png",
+    applyLink: "https://www2.deloitte.com/careers"
+  },
+    {
+    month: "December",
+    company: "Whipro",
+    name:"coderzz",
+    role: "Consulting Analyst",
+    type: "Placement",
+    eligibility:"2nd/3rd year",
+    status: "Upcoming",
+    logo: "assets/logos/wipro.png",
+    applyLink: "https://www2.deloitte.com/careers"
+  },
+];
+
+function filterMonth(selectedMonth) {
+  const container = document.getElementById("cards-container");
+  container.innerHTML = "";
+
+  const filtered = data.filter(
+    item => item.month.toLowerCase() === selectedMonth.toLowerCase()
+  );
+
+  filtered.forEach(item => {
+    const card = document.createElement("div");
+    card.className = "card";
+
+    const normalizedStatus = item.status.toLowerCase();
+
+    const statusClass =
+      normalizedStatus === "open"
+        ? "status-open"
+        : normalizedStatus === "closed"
+        ? "status-closed"
+        : "status-upcoming";
+
+    card.innerHTML = `
+      <div class="card-header">
+        <img 
+          src="${item.logo}" 
+          alt="${item.company} logo"
+          class="company-logo"
+          onerror="this.src='assets/logos/default.png'"
+        />
+        <h3 class="company-name">${item.company}</h3>
+      </div>
+
+      <p><i class="fa-solid fa-code"></i> <strong>Name:</strong> ${item.name}</p>
+      <hr>
+      <p><i class="fa-solid fa-laptop"></i> <strong>Role:</strong> ${item.role}</p>
+      <hr>
+      <p><i class="fa-solid fa-briefcase"></i> <strong>Type:</strong> ${item.type}</p>
+      <hr>
+      <p><i class="fa-solid fa-graduation-cap"></i> <strong>Eligibility:</strong> ${item.eligibility}</p>
+      <hr>
+
+      <p class="${statusClass}">● ${item.status}</p>
+
+      <a href="${item.applyLink || '#'}" target="_blank">
+        <button class="apply-btn">Apply Now</button>
+      </a>
+    `;
+
+    container.appendChild(card);
+  });
+}
+function selectMonth(button, month) {
+  // Remove active from all month buttons
+  document.querySelectorAll(".month-btn").forEach(btn =>
+    btn.classList.remove("active")
+  );
+
+  // Add active to clicked button
+  button.classList.add("active");
+
+  // Load cards for selected month
+  filterMonth(month);
+}
+window.onload = function () {
+  const januaryBtn = document.querySelector(
+    ".month-btn[onclick*='January']"
+  );
+
+  januaryBtn.classList.add("active");
+  filterMonth("January");
+};
